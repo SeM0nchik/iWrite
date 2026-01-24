c5da0002bb35180c8b15c87faa6cd1e19cd581af
SeM0nchik <semone.pavlychev@gmail.com>
2026-01-24 23:21:10 +0300

    update(urls): added new url route

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('category/<str:slug>', PostFromCategory.as_view(), name='post_by_category'),
     path('rating/', RatingCreateView.as_view(), name='rating'),
     path('post/my/', PostByUserListView.as_view(), name='my_posts'),
-
 ]
